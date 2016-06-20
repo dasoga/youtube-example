08f5e3bf722c7152b3725dcb93e1f43ea6bea1a0
Dante Solorio <ddasogaa@gmail.com>
2016-06-20 10:46:07 -0500

    Get data from json online

--- YouTube/HomeViewController.swift
+++ YouTube/HomeViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class HomeViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
+    /*
     var videos: [Video] = {
         var exampleChannel = Channel()
         exampleChannel.name = "Example Channel Name"
@@ -30,10 +31,51 @@ class HomeViewController: UICollectionViewController, UICollectionViewDelegateFl
         
         return [blankSpaceVideo, otherVideo]
     }()
+    */
+    
+    var videos: [Video]?
+    
+    func fetchVideos(){
+        let url = NSURL(string: "https://s3-us-west-2.amazonaws.com/youtubeassets/home.json")
+        NSURLSession.sharedSession().dataTaskWithURL(url!){ (data, response, error ) in
+            if error != nil{
+                print(error)
+                return
+            }
+            
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers)
+                self.videos = [Video]()
+                
+                for dictionary in json as! [[String: AnyObject]]{
+                    let video = Video()
+                    
+                    video.title = dictionary["title"] as? String
+                    video.thumbnailImageName = dictionary["thumbnail_image_name"] as? String
+                    
+                    let channelDictionary = dictionary["channel"] as! [String:AnyObject]
+                    let channel = Channel()
+                    channel.name = channelDictionary["name"] as? String
+                    channel.profileImageName = channelDictionary["profile_image_name"] as? String
+                    
+                    video.channel = channel
+                    self.videos?.append(video)
+                }
+                
+                self.collectionView?.reloadData()
+                
+            }catch let jsonError{
+                print(jsonError)
+            }
+        }.resume()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        fetchVideos()
+        
+        navigationItem.title = "Home"
         navigationController?.navigationBar.translucent = false
         
         let titleLabel = UILabel(frame: CGRectMake(0,0,view.frame.width - 32, view.frame.height))
@@ -83,12 +125,12 @@ class HomeViewController: UICollectionViewController, UICollectionViewDelegateFl
     
     // MARK: - Collection View functions
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return videos.count
+        return videos?.count ?? 0
     }
     
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cellId", forIndexPath: indexPath) as! VideoCell
-        cell.video = videos[indexPath.item]
+        cell.video = videos?[indexPath.item]
         return cell
     }
     
